7f9a1ad711d56b0a35d7a66b7949056dae964a68
matpoz <mateusz.pozimski@gmail.com>
2020-11-09 20:36:22 +0100

    label for input - accesibility x1000

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,16 +3,13 @@ import React from "react";
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
-      <form action="">
-        <label htmlFor="searchbox" />
-        <input
-          id="searchbox"
-          className="pa3 ba b--green bg-lightest-blue"
-          type="search"
-          placeholder="search robots"
-          onChange={searchChange}
-        />
-      </form>
+      <input
+        aria-label="search robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
     </div>
   );
 };
